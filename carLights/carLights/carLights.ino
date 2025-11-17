09ea512e2d897cffce467da45417cb23244a27cb
rgarro <rgarro@gmail.com>
2025-11-17 16:51:28 -0600

    haunted hearth che baker...

---
+++ carLights/carLights/carLights.ino
@@ -0,0 +1,23 @@
+/**
+ *        _______
+ *        //  ||\ \
+ *  _____//___||_\ \___
+ *  )  _          _    \
+ *  |_/ \________/ \___|
+ * ___\_/________\_/______
+ * Proyecto Final del curso de Electricidad automotriz
+ * Simulador de lUCES de automovil
+ *
+ *
+ *
+ *@author Rolando<rgarro@gmail.com> <https://emptyart.github.io/>
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
