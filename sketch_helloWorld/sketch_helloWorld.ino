7146cc3882e29e9edc838c25e66dcd8f701390a6
rgarro <rgarro@gmail.com>
2025-11-18 15:43:31 -0600

    cofee break ..

---
+++ sketch_helloWorld/sketch_helloWorld.ino
@@ -0,0 +1,23 @@
+/**
+* __________________|      |____________________________________________
+*      ,--.    ,--.          ,--.   ,--.
+*     |oo  | _  \  `.       | oo | |  oo|
+* o  o|~~  |(_) /   ;       | ~~ | |  ~~|o  o  o  o  o  o  o  o  o  o  o
+*     |/\/\|   '._,'        |/\/\| |/\/\|
+* __________________        ____________________________________________
+*                   |      |BlinkingLeds SA
+*
+*
+*
+*@author Rolando <rgarro@gmail.com> <https://emptyart.github.io/>
+*/
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("gol de saprissa ...");
+  //delay(1000);
+}
